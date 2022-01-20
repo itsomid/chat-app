b3ba1b76d69620fae8513810867a2de30a6021b7
Omid <o.shabani@hotmail.com>
2022-01-20 22:40:29 +0330

    implement socket.io

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
+const http =  require('http')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-module.exports = app
+module.exports = {server, io}
