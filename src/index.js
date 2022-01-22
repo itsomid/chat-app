ac075579ba2fa22c86937a5fa947e7f6448b342a
Omid <o.shabani@hotmail.com>
2022-01-22 17:33:40 +0330

    implement simple counter application to test socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-const {server, io} = require('./app')
+const { server, io } = require('./app')
 const port = process.env.PORT
 
-io.on('connection',()=>{
+let count = 0
+
+io.on('connection', (socket) => {
     console.log('New websocket connection');
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++;
+
+        //emit the event to specific connection
+        // socket.emit('countUpdated',count)
+
+        //emit the event to every single connection
+        io.emit('countUpdated',count)
+    })
 })
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`server is up on port: ${port}`);
 })
