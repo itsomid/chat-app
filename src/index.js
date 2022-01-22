92ff07a0f10ad5411f6035ebb2deb3c2d3bbf5b1
Omid <o.shabani@hotmail.com>
2022-01-22 18:40:14 +0330

    add simple form to send message through socket.io

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ const { server, io } = require('./app')
 const port = process.env.PORT
 
 let count = 0
+let message = 'Welcome!!'
+
 
 io.on('connection', (socket) => {
     console.log('New websocket connection');
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', message)
 
-    socket.on('increment', () => {
-        count++;
-
-        //emit the event to specific connection
+    socket.on('sendMessage', (msg) => {
+        //emit the event to specific connection 
         // socket.emit('countUpdated',count)
-
-        //emit the event to every single connection
-        io.emit('countUpdated',count)
+        io.emit('message', msg)
     })
+  
 })
 server.listen(port, () => {
     console.log(`server is up on port: ${port}`);
