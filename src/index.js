245e25d5903ed4b92cab6b5204a0c95427fa6802
Omid <o.shabani@hotmail.com>
2022-01-23 13:33:51 +0330

    broadcasting event to other user when a user open/close app

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ io.on('connection', (socket) => {
     console.log('New websocket connection');
 
     socket.emit('message', message)
-
+    socket.broadcast.emit('message', 'A new user has joined!')
     socket.on('sendMessage', (msg) => {
         //emit the event to specific connection 
         // socket.emit('countUpdated',count)
+
+        //emit the event to all connection
         io.emit('message', msg)
     })
-  
+    socket.on('disconnect', () => {
+        io.emit('message','user has left!')
+    })
+
 })
 server.listen(port, () => {
     console.log(`server is up on port: ${port}`);
