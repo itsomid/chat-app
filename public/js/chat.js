df1cb8485994eb6fa537b8ca0a872fbf5207b568
Omid <o.shabani@hotmail.com>
2022-01-27 15:54:00 +0330

    add features to form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,23 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormBtn = $messageForm.querySelector('button')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-
-document.querySelector('#message-form').addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
     e.preventDefault()
+
+    $messageFormBtn.setAttribute('disabled','disabled')
+ 
     const message = e.target.elements.message.value
     socket.emit('sendMessage',message,(error)=>{
+        $messageFormBtn.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         if(error){
             return console.log(error)
         } 
