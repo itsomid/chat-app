98b09c23a6635f192443f922fdca51223af303e8
Omid <o.shabani@hotmail.com>
2022-01-31 21:02:34 +0330

    create join page for enter username and room name and implement style for it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { server, io } = require('./app')
 const port = process.env.PORT
 const Filter = require('bad-words')
-const {generateMessage, generateLocationMessage} = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 io.on('connection', (socket) => {
     console.log('New websocket connection');
@@ -13,20 +13,20 @@ io.on('connection', (socket) => {
         // socket.emit('message',count)
         const filter = new Filter()
 
-        if(filter.isProfane(msg)){
+        if (filter.isProfane(msg)) {
             return callback('profanity is not allowed!')
         }
         //emit the event to all connection
-        io.emit('message', generateMessage(msg)) 
+        io.emit('message', generateMessage(msg))
         callback()
     })
     socket.on('disconnect', () => {
-        io.emit('message',generateMessage('user has left!'))
+        io.emit('message', generateMessage('user has left!'))
     })
 
     //setup a listener
-    socket.on('sendLocation',(coords,callback)=>{
-        io.emit('locationMessage',generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
