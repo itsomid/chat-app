7f479df61738ccb952d3aedaf958cd1eca0ea811
Omid <o.shabani@hotmail.com>
2022-02-01 23:45:12 +0330

    impelement user removing

--- src/utils/user.js
+++ src/utils/user.js
@@ -27,3 +27,32 @@ const addUser = ({id, username, room}) => {
     users.push(user)
     return {user}
 }
+
+const removeUser = (id)=>{
+    const index = users.findIndex((user)=>{
+        return user.id == id
+    })
+    if(index !== -1){
+        return users.splice(index,1)
+    }
+}
+
+addUser({
+    id: 56,
+    username: 'omid',
+    room: 'Blockchain'
+})
+// console.log(users)
+
+const result = addUser({
+    id: 24,
+    username: 'Elahe',
+    room: 'Blockchain'
+})
+
+// console.log(result)
+
+const removedUser = removeUser(56)
+
+console.log(removedUser)
+console.log(users)
